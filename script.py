9cb8f570f5487fe57a1705ba901fcc7b47098eb9
Siphon880gh <weng.f.fung@gmail.com>
2024-06-13 00:51:25 -0700

    Assistant with message thread

--- script.py
+++ script.py
@@ -10,12 +10,14 @@ secret_key = os.getenv('ELEVENLABS_API_KEY')
 api_key = os.getenv('OPENAI_API_KEY')
 org_id = os.getenv('OPENAI_ORG_ID')
 proj_id = os.getenv('OPENAI_PROJ_ID')
+assist_id = os.getenv('OPENAI_ASSISTANT_TRANSLATOR')
 
 print("**************************")
 print("Test .env")
 print("**************************")
 print("api_key: " + api_key)
 print("org_id: " + org_id)
+print("proj_id: " + proj_id)
 print("proj_id: " + proj_id)
 
 # Create an OpenAI client
@@ -24,3 +26,23 @@ client = OpenAI(
   organization=org_id,
   project=proj_id,
 )
+
+
+# Assistant gpt-4-turbo with instructions: You can translate from English to the language of my choice
+# Create assistant at https://platform.openai.com/assistants/
+assistant = client.beta.assistants.retrieve(assist_id)
+# print(my_assistant)
+
+thread = client.beta.threads.create()
+
+def translateTo(language, text): 
+    return f"Please translate this to {language}:\n\n{text}!"
+
+thread = client.beta.threads.create()
+
+message = client.beta.threads.messages.create(
+  thread_id=thread.id,
+  role="user",
+  content=translateTo("French", "Hello, how are you?"), 
+)
+print(message)
