130d86496337f39934c9a10c5e0b46f7c60963cf
Siphon880gh <weng.f.fung@gmail.com>
2024-06-13 00:53:50 -0700

    Executes stream

--- script.py
+++ script.py
@@ -46,3 +46,35 @@ message = client.beta.threads.messages.create(
   content=translateTo("French", "Hello, how are you?"), 
 )
 print(message)
+
+# Read more:
+# https://platform.openai.com/docs/assistants/overview?context=with-streaming
+class EventHandler(AssistantEventHandler):    
+  @override
+  def on_text_created(self, text) -> None:
+    print(f"\nassistant > ", end="", flush=True)
+      
+  @override
+  def on_text_delta(self, delta, snapshot):
+    print(delta.value, end="", flush=True)
+      
+  def on_tool_call_created(self, tool_call):
+    print(f"\nassistant > {tool_call.type}\n", flush=True)
+  
+  def on_tool_call_delta(self, delta, snapshot):
+    if delta.type == 'code_interpreter':
+      if delta.code_interpreter.input:
+        print(delta.code_interpreter.input, end="", flush=True)
+      if delta.code_interpreter.outputs:
+        print(f"\n\noutput >", flush=True)
+        for output in delta.code_interpreter.outputs:
+          if output.type == "logs":
+            print(f"\n{output.logs}", flush=True)
+ 
+
+with client.beta.threads.runs.stream(
+  thread_id=thread.id,
+  assistant_id=assistant.id,
+  event_handler=EventHandler(),
+) as stream:
+  stream.until_done()
